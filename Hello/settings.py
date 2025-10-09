8d5d8c372888912d77f5d916997bcb79ff2ac1a6
Puskar Maity <puskarmaity007@gmail.com>
2025-10-10 00:20:09 +0530

    changes

--- Hello/settings.py
+++ Hello/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+from django.contrib.messages import constants as messages
+from django.contrib import messages
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
--- home/views.py
+++ home/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render,HttpResponse
 from datetime import datetime
+from home.models import Contact
+from django.contrib import messages
+
 
 # Create your views here.
 def index(request):
@@ -16,15 +19,30 @@ def about(request):
     # return HttpResponse("This is about  page ")
     return render(request,'about.html')
 
+
+
 def contact(request):
-    # return HttpResponse("This is nothing page ")
-    #if have request and enter the form then save it in database
-    if request.method=="POST":
-        name=request.POST.get('name')
-        email=request.POST.get('email')
-        phone=request.POST.get('phone')
-        description=request.POST.get('description')
+    message = ""
+    if request.method == "POST":
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        phone = request.POST.get('phone')
+        description = request.POST.get('description')
+        if name and email and phone and description:
+            contact = Contact(name=name, email=email, phone=phone, description=description, date=datetime.today())
+            contact.save()
+            messages.success(request, "Profile details updated.")
+            message = "Your message has been sent!"
+        else:
+            message = "Please fill all fields."
+
+    return render(request, 'contact.html', {'message': message})
+
+
         
 
 
     return render(request,'contact.html')
+
+
+    
